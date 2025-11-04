b76068da4d98a8e0c23a92e3373c32711677137f
bharatroy4D <bharatroy1005@gmail.com>
2025-11-04 10:57:49 +0600

    Active route is set

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,33 @@
+"use client"
 import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
 const Header = () => {
+    const PathName = usePathname();
+    const router = useRouter();
+    const handler = () =>{
+        router.push("/login")
+    }
     const NavLink = [
-        {"name": "Home", "link":"/home"},
-        {"name": "About", "link":"/about"},
-        {"name": "Contact", "link":"/contact"},
-        {"name": "Blogs", "link":"/blogs"},
+        { "name": "Home", "link": "/home" },
+        { "name": "About", "link": "/about" },
+        { "name": "Contact", "link": "/contact" },
+        { "name": "Blogs", "link": "/blogs" },
     ]
     return (
         <div className='bg-sky-700'>
             <div className='flex items-center justify-between py-3 px-10 mx-auto'>
-                <h1 className='text-3xl font-bold text-gray-900'>Logo</h1>
-                <ul className='flex items-center gap-10 text-lg text-gray-900'>
-                {
-                    NavLink.map(nav => (
-                        <div key={nav.link}>
-                            <Link href={nav.link}>{nav.name}</Link>
-                        </div>
-                    ))
-                }
+                <h1 className='text-3xl font-bold text-white'>Logo</h1>
+                <ul className='flex items-center gap-10 text-lg text-white'>
+                    {
+                        NavLink.map(nav => (
+                            <div key={nav.link}>
+                                <Link className={`${PathName == nav.link && "text-yellow-400"}`} href={nav.link}>{nav.name}</Link>
+                            </div>
+                        ))
+                    }
+                    <button onClick={handler}>Login</button>
                 </ul>
             </div>
         </div>
