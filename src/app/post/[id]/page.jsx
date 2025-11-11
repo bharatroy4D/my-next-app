c1e5eac94cf4aad9aab5b012157dcf6a41e67d1e
bharatroy4D <bharatroy1005@gmail.com>
2025-11-11 21:29:50 +0600

    Dynamic details page design

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 
 const getDetailsPage = async (id) => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-    cache: "no-store", // 🚀 dynamic fresh data
+    // cache: "no-store", 
   });
   return res.json();
 };
 
-export default async function Page({ params }) {
+const Page = async ({ params }) => {
   const { id } = await params; // ✅ unwrap params first
   const post = await getDetailsPage(id);
 
   return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold mb-3">Post Title: {post.title}</h1>
-      <p className="text-lg text-gray-600">Post Description: {post.body}</p>
-    </div>
+   <div className="p-10 m-10 h-screen w-3xl mx-auto bg-white rounded-xl shadow-md border border-gray-600 hover:shadow-lg transition-all duration-300">
+  <h1 className="text-4xl font-semibold text-gray-800 mb-4 leading-tight capitalize">
+    {post.title}
+  </h1>
+  <p className="text-lg text-gray-600 leading-relaxed tracking-wide">
+    {post.body}
+  </p>
+</div>
+
   );
 }
+export default Page;
