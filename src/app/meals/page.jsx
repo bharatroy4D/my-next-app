c7a6f46d7b378b66a410759860bc6a84c4b4430d
bharatroy4D <bharatroy1005@gmail.com>
2025-11-11 22:29:51 +0600

    Next project into Meals route and meals page create

---
+++ src/app/meals/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MealPage = () => {
+    return (
+        <div>
+            this meals page
+        </div>
+    );
+};
+
+export default MealPage;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,6 +15,7 @@ const Header = () => {
         { "name": "Contact", "link": "/contact" },
         { "name": "Blogs", "link": "/blogs" },
         { "name": "Post", "link": "/post" },
+        { "name": "Meals", "link": "/meals" },
     ]
     if(PathName.includes("dashboard")) return <div className='bg-yellow-200 py-3'>Dashboard layout</div>
     return (
